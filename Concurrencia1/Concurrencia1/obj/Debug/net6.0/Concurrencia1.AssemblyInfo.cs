3e0d23c5e88059fa6aa40432da6003aab9cb4d6f
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-11-19 10:02:20 -0600

    Add files via upload

--- Concurrencia1/Concurrencia1/obj/Debug/net6.0/Concurrencia1.AssemblyInfo.cs
+++ Concurrencia1/Concurrencia1/obj/Debug/net6.0/Concurrencia1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Concurrencia1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd82bf0b1f6e4dd2f9a7017277f24f0bf37106d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc82d87f0b09a49e3e5633b8d7dd665ec6d9ef6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Concurrencia1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Concurrencia1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
