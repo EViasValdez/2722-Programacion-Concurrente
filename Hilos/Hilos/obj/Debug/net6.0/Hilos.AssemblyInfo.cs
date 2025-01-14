d07900b4a9b29913ebaca6ff580ddfb22aa76ad9
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-01-13 21:09:54 -0600

    Add files via upload

--- Hilos/Hilos/obj/Debug/net6.0/Hilos.AssemblyInfo.cs
+++ Hilos/Hilos/obj/Debug/net6.0/Hilos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hilos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af88e9c5a6dcea99d87ae4d021b9f93eb7ad9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7b228ea336f19303cd7bc6cda4d29ccf570d50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hilos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hilos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
